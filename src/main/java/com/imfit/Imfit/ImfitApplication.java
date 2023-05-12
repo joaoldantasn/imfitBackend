32a991227575f1be1400c57463f598708fccd2e2
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2023-05-12 16:04:50 -0300

    first commit

---
+++ src/main/java/com/imfit/Imfit/ImfitApplication.java
@@ -0,0 +1,13 @@
+package com.imfit.Imfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImfitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imfit/Imfit/ImfitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imfit.Imfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImfitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
