c957acd09fb181d34fa93d2cfa3182052c82261b
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2023-05-12 18:37:27 -0300

    seed teste base de dados

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,14 @@
+INSERT INTO tb_treino (name) VALUES ('Treino A');
+INSERT INTO tb_treino (name) VALUES ('Treino B');
+
 INSERT INTO tb_exercicio (name, muscle_group, img_url, short_description) VALUES ('Supino', 'Peitoral', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Ftreinomestre.com.br%2Fsupino-fechado-tudo-o-que-voce-precisa-saber%2F&psig=AOvVaw0dsMBYrwV474IX31h7uMBq&ust=1684005730875000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCLD_w63A8P4CFQAAAAAdAAAAABAE', 'Deite no banco, monte a barra e utilize força para pressionar o músculo do peito');
 INSERT INTO tb_exercicio (name, muscle_group, img_url, short_description) VALUES ('Supino', 'Peitoral', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Ftreinomestre.com.br%2Fsupino-fechado-tudo-o-que-voce-precisa-saber%2F&psig=AOvVaw0dsMBYrwV474IX31h7uMBq&ust=1684005730875000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCLD_w63A8P4CFQAAAAAdAAAAABAE', 'Deite no banco, monte a barra e utilize força para pressionar o músculo do peito');
 INSERT INTO tb_exercicio (name, muscle_group, img_url, short_description) VALUES ('Supino', 'Peitoral', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Ftreinomestre.com.br%2Fsupino-fechado-tudo-o-que-voce-precisa-saber%2F&psig=AOvVaw0dsMBYrwV474IX31h7uMBq&ust=1684005730875000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCLD_w63A8P4CFQAAAAAdAAAAABAE', 'Deite no banco, monte a barra e utilize força para pressionar o músculo do peito');
 INSERT INTO tb_exercicio (name, muscle_group, img_url, short_description) VALUES ('Supino', 'Peitoral', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Ftreinomestre.com.br%2Fsupino-fechado-tudo-o-que-voce-precisa-saber%2F&psig=AOvVaw0dsMBYrwV474IX31h7uMBq&ust=1684005730875000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCLD_w63A8P4CFQAAAAAdAAAAABAE', 'Deite no banco, monte a barra e utilize força para pressionar o músculo do peito');
 INSERT INTO tb_exercicio (name, muscle_group, img_url, short_description) VALUES ('Supino', 'Peitoral', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Ftreinomestre.com.br%2Fsupino-fechado-tudo-o-que-voce-precisa-saber%2F&psig=AOvVaw0dsMBYrwV474IX31h7uMBq&ust=1684005730875000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCLD_w63A8P4CFQAAAAAdAAAAABAE', 'Deite no banco, monte a barra e utilize força para pressionar o músculo do peito');
+
+INSERT INTO tb_pertecimento (treino_id, exercicio_id, series, repeticoes) VALUES (1, 1, 4, 10);
+INSERT INTO tb_pertecimento (treino_id, exercicio_id, series, repeticoes) VALUES (1, 2, 4, 10);
+INSERT INTO tb_pertecimento (treino_id, exercicio_id, series, repeticoes) VALUES (1, 3, 4, 10);
+INSERT INTO tb_pertecimento (treino_id, exercicio_id, series, repeticoes) VALUES (1, 4, 4, 10);
+INSERT INTO tb_pertecimento (treino_id, exercicio_id, series, repeticoes) VALUES (1, 5, 4, 10);
